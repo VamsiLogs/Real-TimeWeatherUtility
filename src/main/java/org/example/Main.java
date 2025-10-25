e743787cb952fac4ab55194bf322f263a5c24126
Dileep Hegde <dileephegde96@gmail.com>
2025-10-25 20:17:27 +0530

    Add Weather Overview feature using OpenWeather API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     static void main() {
-        // This is testing branch merging to dev-branch
-
+        OpenWeatherApiCall apiCall = new OpenWeatherApiCall();
+        double lat = 37.7749;
+        double lon = -122.4194;
+        apiCall.getWeatherOverview(lat, lon);
     }
 }
